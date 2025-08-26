a6c2fa7276b30d9d437d49eef3e34ab1ab48a1ba
Zain Zafar <zanzaf@gmail.com>
2025-08-27 04:32:33 +0500

    Refactor App to add providers and RootNavigator
    Replaces Root with RootNavigator and wraps the app with ThemeProvider, AuthProvider, Redux Provider, and PersistGate for state management and theming. Also updates StatusBar configuration for improved appearance.

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -9,9 +9,14 @@ import { StatusBar, useColorScheme } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import Root from './src/Root';
+import { ThemeProvider } from './src/theme/ThemeProvider';
+import { AuthProvider } from './src/auth/AuthProvider';
+import RootNavigator from './src/navigation/RootNavigator';
 import { ApolloProvider } from '@apollo/client/react';
 import { apolloClient } from './src/graphql/client';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from './src/store';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -19,11 +24,23 @@ function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <SafeAreaProvider>
-        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+        <StatusBar 
+          barStyle={isDarkMode ? 'light-content' : 'dark-content'} 
+          backgroundColor={isDarkMode ? '#000000' : '#ffffff'}
+          translucent={false}
+        />
         <NavigationContainer theme={isDarkMode ? DarkTheme : DefaultTheme}>
-          <ApolloProvider client={apolloClient}>
-            <Root />
-          </ApolloProvider>
+          <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+              <ApolloProvider client={apolloClient}>
+                <ThemeProvider>
+                  <AuthProvider>
+                    <RootNavigator />
+                  </AuthProvider>
+                </ThemeProvider>
+              </ApolloProvider>
+            </PersistGate>
+          </Provider>
         </NavigationContainer>
       </SafeAreaProvider>
     </GestureHandlerRootView>
